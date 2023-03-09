ceb2a119fe0f19489b1837c4f998053c60bbf9bd
gacmen45 <marekgacek45@gmail.com>
2023-03-09 07:48:23 +0100

    initial commit

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require('../src/header/header.php');
+
+    ?>
+
+<main>
+
+    <p>test</p>
+</main>
+
+
+<?php
+
+require('../src/footer/footer.php');
+
+    ?>
---
+++ src/footer/footer.php
@@ -0,0 +1,6 @@
+<h2>footer</h2>
+
+</body>
+
+
+</html>
