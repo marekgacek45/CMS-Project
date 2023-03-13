ae850f90b960805c2763cc8cbcc0063d8184e9cb
marekgacek45 <marekgacek45@gmail.com>
2023-03-13 09:00:30 +0100

    add style to pagination

--- includes/pagination.php
+++ includes/pagination.php
@@ -1,26 +1,45 @@
-<div>
-    <ul>
-        <li>
-            <?php if ($paginator->previous): ?>
-                <a href="?page=<?= $paginator->previous ?>">previous</a>
-            <?php else: ?>
-                previous
-            <?php endif ?>
-        </li>
-        <li>
-            <?php if ($paginator->next): ?>
-                <a href="?page=<?= $paginator->next ?>">next</a>
-            <?php else: ?>
-                next
-            <?php endif ?>
-        </li>
-    </ul>
+<nav aria-label="Page navigation example">
+    <ul class="pagination">
+        <?php if ($paginator->previous): ?>
+            <li class="page-item">
+                <a class="page-link" aria-label="Previous" href="?page=<?= $paginator->previous ?>"> <span
+                        aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span></a>
+            </li>
+        <?php else: ?>
+            <li class="page-item disabled">
+                <a class="page-link" aria-label="Previous" href="#">
+                    <span aria-hidden="true">&laquo;</span>
+                    <span class="sr-only">Previous</span></a>
+                </a>
+            </li>
+        <?php endif ?>
 
-    <ul>
         <?php for ($i = 1; $i <= $paginator->totalPages; $i++): ?>
-            <li>
+            <li class="page-link" class="page-item">
                 <a href="?page=<?= $i ?>"> <?= $i ?></a>
             </li>
         <?php endfor ?>
+
+
+        <?php if ($paginator->next): ?>
+            <li class="page-item">
+                <a class="page-link" aria-label="Next" href="?page=<?= $paginator->next ?>"> <span
+                        aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span></a>
+            </li>
+        <?php else: ?>
+            <li class="page-item disabled">
+                <a class="page-link " aria-label="Next" href="#">
+                    <span aria-hidden="true">&raquo;</span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </li>
+        <?php endif ?>
+
+
+
     </ul>
-    </div>
+
+
+</nav>
